59d211c0131689590e1a8b4b4312803a942d5a78
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-01 15:32:39 +0000

    Add file 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if i == len(str) - 1:
+            print("{}".format(chr(ord(str[i]) - 32) if ord(
+                str[i]) in range(97, 123) else str[i]))
+        else:
+            print("{}".format(chr(ord(str[i] - 32) if ord(
+                str[i] in range(97, 123) else str[i]), end='')
