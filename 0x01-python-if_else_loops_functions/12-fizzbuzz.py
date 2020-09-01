df07c75db4a055173809c46ac60e4233896bb357
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-01 15:08:41 +0000

    Add file 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/puthon3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", " ", end="")
+        elif i % 5 == 0 and i != 100:
+            print("Buzz", " ", end="")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", " ", end="")
+        elif i == 100:
+            print("Buzz", end="\n")
+        else:
+            print(i, " ", end="")
