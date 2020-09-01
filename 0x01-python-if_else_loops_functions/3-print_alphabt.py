21d90f09d113b96231f9d7e84eaa6e934a51b54b
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-01 12:06:41 +0000

    Fix file 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(*["%c" % a for a in range(ord('a'),ord('z')+1) if a not in (ord('q'),ord('e'))],sep='',end='')
+print(*["%c" % a for a in range(ord('a'), ord('z')+1) if a not in (
+    ord('q'), ord('e'))], sep='', end='')
