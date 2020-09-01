3c7552a1044b7a5aaff9bc2b1904038292056605
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-01 15:52:00 +0000

    Fix file 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print(*["%c" % a for a in range(ord('a'), ord('z')+1) if a not in (
-    ord('q'), ord('e'))], sep='', end='')
+for c in range(97, 123):
+    if(chr(c) != 'q' and chr(c) != 'e'):
+        print("{}".format(chr(c)), end="")
