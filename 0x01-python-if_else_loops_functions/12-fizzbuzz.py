7cd4c1076df53c71e32c51907cb976ba09cb6893
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-01 16:15:01 +0000

    Fix file 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            return "FizzBuzz"
-        elif i % 3 == 0:
-            return "Fizz"
-        elif i % 5 == 0:
-            return "Buzz"
-        else:
-            return i
+    if __name__ == "__main__":
+        for i in range(1, 101):
+            if i % 3 == 0 and i % 5 == 0:
+                return "FizzBuzz"
+            elif i % 3 == 0:
+                return "Fizz"
+            elif i % 5 == 0:
+                return "Buzz"
+            else:
+                return i
