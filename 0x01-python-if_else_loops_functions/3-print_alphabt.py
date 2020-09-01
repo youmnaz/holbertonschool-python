0a4ef3241f4c86bed2bd76262018614216bd53e1
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-01 10:28:02 +0000

    Fix file 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for letter_code in range(ord('a'), ord('z')+1):
-    letter = chr(letter_code)
-    if letter not in "qe":
-        print(letter, end="")
+print(*["%c" % a for a in range(ord('a'),ord('z')+1) if a not in (ord('q'),ord('e'))],sep='',end='')
