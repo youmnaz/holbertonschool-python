4b7e2008a03c15683f60c938491ba1b8ce00d813
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-03 11:34:44 +0000

    Add file 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    len(sys.argv)
+    for i in range(len(sys.argv)):
+        if i == 0:
+            print("0 arguments.")
+        elif i == 1:
+            print(i, ":", "argument", "/n", "i", ":", "argv", end="")
+        else:
+            print(i, ":", "arguments", "/n", "i", ":", "argv", end="/n")
