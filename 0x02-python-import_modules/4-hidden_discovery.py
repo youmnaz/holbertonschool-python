dd9483eb87fd97d8d7d2f54524b21f6b361a76eb
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-03 14:13:20 +0000

    Fix file 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print("\n".join(dir()))
+    for i in dir(hidden_4):
+        if i[0] != "_":
+            print("\n".join(dir(hidden_4)))
