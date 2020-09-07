7b2e3a7d5336ff53785445803e3a288afdfb89b5
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-07 11:25:05 +0000

    Fix file 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord("c"): None})
-    second_string = new_string.translate({ord("C"): None})
-    print(second_string)
+    new_string = ""
+    for i in my_string:
+        if i != "c" and i != "C":
+            new_string += i
+    return new_string
