d0fb15cb84dfd16b50f17c4986e59f9e927da164
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-08 14:53:10 +0000

    Fix file 1100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    mul = 0
-    div = 0
-    average = 0.0
-    for i in my_list:
-        mul += i[0] * i[1]
-        div += i[1]
-    average = mul / div
-    return average
+    if len(my_list) != 0:
+        mul = 0
+        divide = 0
+        average = 0.0
+        for i in my_list:
+            mul += i[0] * i[1]
+            divide += i[1]
+            average = mul / divide
+        return average
+    else:
+        return 0
