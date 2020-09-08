37797b016b8d2cd97c5eacaa43a7bb2e5683e869
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-08 11:27:37 +0000

    Fix file 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = [None] * len(my_list)
     for n, i in enumerate(my_list):
         if i == search:
-            my_list[n] = replace
-    return my_list
+            new_list[n] = replace
+        else:
+            new_list[n] = my_list[n]
+    return new_list
