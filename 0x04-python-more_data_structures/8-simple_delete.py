cbe7fb31efd8a7f77731e850d20d369ce2142cba
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-08 12:24:34 +0000

    Add file 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+        print(a_dictionary)
+    else:
+        print(a_dictionary)
