f98fe095908c8fd9084b79ea4d98e24cb4842aa1
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-10 10:19:38 +0000

    Add file 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    while x <= len(my_list):
+    for i in range(0, x):
         try:
-            print(*my_list[:x], sep='')
-            break
+            print("{}".format(my_list[i]), end="")
+            i += 1
         except ValueError:
-            print("x not in range")
+            print("not in range")
