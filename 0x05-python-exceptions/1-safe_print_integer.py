b523c927647df73f16534edca3853a18f396350f
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-10 11:45:35 +0000

    Fix file 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
+        if int(value):
             print("{:d}".format(value))
             return True
     except:
