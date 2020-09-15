c18b1a12448b960d96e6b9481d7798563c95e8b1
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-15 07:14:19 +0000

    Fix file 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+    2-square.py
+    Module that defines a Square
+    return size
+"""
+
+
 class Square:
+    """ This is a class that defines a Square
+        return size
+    """
     def __init__(self, size=0):
+        """ Function that checks the input type
+            return size
+        """
         self.__size = size
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
