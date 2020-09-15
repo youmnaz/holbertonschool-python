aceb0f85994baf0eb237d99431b2aaa9428c8762
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-15 06:55:26 +0000

    Fix file 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+    0-square.py
+    Module that defines a Square
+    return {}
+"""
 class Square:
-    """This is An Empty Class that defines a Square
-
+    """ This is An Empty Class that defines a Square
+        return empty block
     """
     pass
