87d971b39ae6dc205ea83906861a633910bb2544
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-15 07:05:44 +0000

    Fix file 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
 class Square:
+    """ This is a class that defines a Square
+        return size
+    """
     def __init__(self, size):
+        """ Function that gets the size
+            return size
+        """
         self.__size = size
