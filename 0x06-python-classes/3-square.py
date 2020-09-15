a42897a6f277420a0578aeeae3778ac5539badf8
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-15 07:18:45 +0000

    Fix file 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+    3-square.py
+    Module that defines a Square
+    return size
+"""
+
+
 class Square:
+    """ This is a class that defines a Square
+        return size
+    """
     def __init__(self, size=0):
+        """ Function that checks the input type
+            return size
+        """
         self.__size = size
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
@@ -8,4 +21,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """ Function that calculates the area
+            return areq
+        """
         return self.__size ** 2
