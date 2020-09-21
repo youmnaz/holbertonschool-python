82e978def4cda5592ce70881c1af73ff5097d62d
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-09-21 09:37:16 +0000

    Add file 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    0-rectangle.py
+    Module that defines a Rectangle
+    return {}
+"""
+
+
+class Rectangle:
+    """ Empty class that defines a rectangle
+        return None
+    """
+    pass
