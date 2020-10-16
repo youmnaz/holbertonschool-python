b7b8014cc235738f33d2d40cef8e056a1699db44
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-10-16 07:35:14 +0000

    add file 1

---
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+ !/usr/bin/python3
+"""
+    1-hbtn_header.py
+"""
+
+from urllib import (request)
+from sys import argv
+
+if __name__ == "__main__":
+    req = request.Request(argv[1])
+    with request.urlopen(req) as response:
+        header = response.info()
+
+    print(header.get("X-Request-Id"))
