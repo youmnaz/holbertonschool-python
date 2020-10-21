a5ce4cef8614786761a1ef3bf52c1f8e42975d59
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-10-21 10:41:46 +0000

    Add folder 12

---
+++ 0x12-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- updates the score of Bob to 10 in the table second_table.
+-- Holberton School
+UPDATE `second_table` SET `score` = '10' WHERE `second_table`.`name` ='Bob';
---
+++ 0x12-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+This project is to learn SQL basics
