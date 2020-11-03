b39da729210697b7b226d02685a64df6508d8ba6
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-11-03 12:36:57 +0000

    add file 9

---
+++ 0x17-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 9-insert_school.py """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document
+    in a collection based on kwargs
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
