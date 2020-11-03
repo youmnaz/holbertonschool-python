899c15f08e395d8fdec64b06e36bdeb37f3e55da
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-11-03 13:04:53 +0000

    add file

---
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""11-schools_by_topic.py"""
+
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    return mongo_collection.find({"topics": topic})
