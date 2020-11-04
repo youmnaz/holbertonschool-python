17e88fc1c9e497334ae19f0ec00ff7d6f6a30729
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-11-04 08:23:51 +0000

    fix file

--- 0x18-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x18-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,18 +2,18 @@
 """ 0-rotate_2d_matrix.py """
 
 
-def rotate_2d_matrix(mat):
+def rotate_2d_matrix(matrix):
     """
     Given an n x n 2D matrix, rotate it 90 degrees.
     """
-    n = len(mat)
+    n = len(matrix)
     for x in range(int(n / 2)):
         offset = 0
         i = n - 1 - x
         for y in range(x, n - 1 - x):
-            top = mat[x][y]
-            mat[x][y] = mat[i - offset][x]
-            mat[i - offset][x] = mat[i][i - offset]
-            mat[i][i - offset] = mat[y][i]
-            mat[y][i] = top
+            top = matrix[x][y]
+            matrix[x][y] = matrix[i - offset][x]
+            matrix[i - offset][x] = matrix[i][i - offset]
+            matrix[i][i - offset] = matrix[y][i]
+            matrix[y][i] = top
             offset += 1
