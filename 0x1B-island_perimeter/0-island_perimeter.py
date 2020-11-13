137f4842d9676304325f4944475b7149540aba9e
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-11-13 10:14:27 +0000

    update island

--- 0x1B-island_perimeter/0-island_perimeter.py
+++ 0x1B-island_perimeter/0-island_perimeter.py
@@ -5,27 +5,20 @@
 
 
 def island_perimeter(grid):
-    """ returns the perimeter of the island described in grid """
-    perimeter = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
+    """ Returns the perimeter of the island described in grid """
+    if len(grid) == 0 or len(grid[0]) == 0:
+        return 0
+
+    rows_len = len(grid)
+    column_len = len(grid[0])
+    row_set = set()
+    col_set = set()
+
+    for row in range(rows_len):
+        for col in range(column_len):
             if grid[row][col] == 1:
-                if row != 0 and row != len(grid) - 1:
-                    if grid[row - 1][col] == 0:
-                        perimeter += 1
-                    if grid[row + 1][col] == 0:
-                        perimeter += 1
-                if row == 0 and grid[row + 1][col] == 0:
-                    perimeter += 1
-                if row == len(grid) - 1 and grid[row - 1][col] == 0:
-                    perimeter += 1
-                if col != 0 and col != len(grid[row]) - 1:
-                    if grid[row][col - 1] == 0:
-                        perimeter += 1
-                    if grid[row][col + 1] == 0:
-                        perimeter += 1
-                if col == 0 and grid[row][col + 1] == 0:
-                    perimeter += 1
-                if col == len(grid[row]) - 1 and grid[row][col - 1] == 0:
-                    perimeter += 1
-    return perimeter
+                row_set.add(row)
+                col_set.add(col)
+
+    result = (len(row_set) * 2) + (len(col_set) * 2)
+    return result
