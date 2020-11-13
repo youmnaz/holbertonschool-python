d6597bb57472b67e67bd4c974285265a26bbd5d4
Youmna Zogheib <youmna.zogheib@gmail.com>
2020-11-13 10:41:26 +0000

    update island

--- 0x1B-island_perimeter/0-island_perimeter.py
+++ 0x1B-island_perimeter/0-island_perimeter.py
@@ -6,19 +6,28 @@
 
 def island_perimeter(grid):
     """ Returns the perimeter of the island described in grid """
-    if len(grid) == 0 or len(grid[0]) == 0:
-        return 0
-
-    rows_len = len(grid)
-    column_len = len(grid[0])
-    row_set = set()
-    col_set = set()
-
-    for row in range(rows_len):
-        for col in range(column_len):
-            if grid[row][col] == 1:
-                row_set.add(row)
-                col_set.add(col)
-
-    result = (len(row_set) * 2) + (len(col_set) * 2)
-    return result
+    d = 0
+    perimeter = 0
+    height = len(grid)
+    length = len(grid[0])
+    for line in grid:
+        c = 0
+        for val in line:
+            if val == 1:
+                surround = 4
+                if c != length - 1:
+                    if grid[d][c + 1] == 1:
+                        surround -= 1
+                if c != 0:
+                    if grid[d][c - 1] == 1:
+                        surround -= 1
+                if d != height - 1:
+                    if grid[d + 1][c] == 1:
+                        surround -= 1
+                if d != 0:
+                    if grid[d - 1][c] == 1:
+                        surround -= 1
+                perimeter += surround
+            c += 1
+        d += 1
+    return perimeter
